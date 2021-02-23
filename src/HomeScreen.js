008fc205633360d5bd405e2dac6c2c9a237257db
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-02-23 16:30:42 -0500

    Building Netflix Clone HomeScreen

---
+++ src/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className="home__screen">
+      <h1>Homescreen</h1>
+    </div>
+  );
+}
+
+export default HomeScreen;
