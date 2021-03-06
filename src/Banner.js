14ff66d1aa0640469a6a6c955a978f1d2434ba92
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-05 20:25:36 -0500

    Completed Building Netflix Clone Banner

--- src/Banner.js
+++ src/Banner.js
@@ -2,7 +2,18 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
-  return <div></div>;
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://getyourcomicon.co.uk/wp-content/uploads/2020/07/Lucifer_Seaosn_5_Banner_1.jpg")`,
+        backgroundPosition: "center center",
+      }}
+    >
+      <div className="banner__contents"></div>
+    </header>
+  );
 }
 
 export default Banner;
