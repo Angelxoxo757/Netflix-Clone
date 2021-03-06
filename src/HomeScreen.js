c82453c0509025b6b6a6cb31ca553a7c7b45f2e2
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-06 18:02:42 -0500

    Completed Building Netflix Clone Rows

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
+      <Row />
       {/* ROWS */}
     </div>
   );
