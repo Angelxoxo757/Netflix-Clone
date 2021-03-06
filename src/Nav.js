6b6d061983c9b8f9acaf86a94672db641d9efcf1
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-05 19:46:22 -0500

    Completed Building Netflix Clone Navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 function Nav() {
-    return (
-        <div className="nav">
-            
-        </div>
-    )
+  // USING BEM
+  const [show, handleShow] = useState(false);
+
+  const trasitionNavbar = () => {
+    if (window.scrollY > 100) {
+      handleShow(true);
+    } else {
+      handleShow(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", trasitionNavbar);
+    return () => window.removeEventListener("scroll", trasitionNavbar);
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <div className="nav__contents">
+        <img
+          className="nav__logo"
+          src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://i0.wp.com/jimheath.tv/wp-content/uploads/2019/04/lucifer-season-4-netflix.jpg?ssl=1"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Nav
+export default Nav;
