f5a30b5a49ee585269e82523865869b87ffefea0
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-05 21:06:07 -0500

    Completed Building Netflix Clone Banner

--- src/Banner.js
+++ src/Banner.js
@@ -11,7 +11,16 @@ function Banner() {
         backgroundPosition: "center center",
       }}
     >
-      <div className="banner__contents"></div>
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">This is a test description</h1>
+      </div>
+
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
