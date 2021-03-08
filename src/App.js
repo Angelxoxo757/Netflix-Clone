2b414ad280b6b6fbdd7f33e62acab47095ef7683
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-08 16:25:47 -0500

    Building Netflix Clone LoginScreen

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
   return (
     // BEM
     <div className="app">
-      <HomeScreen />
       <Router>
         {!user ? (
           <LoginScreen />
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,7 +4,13 @@ import "./LoginScreen.css";
 function LoginScreen() {
   return (
     <div className="loginScreen">
-      <div className="loginScreen__background"></div>
+      <div className="loginScreen__background">
+        <img
+          className="loginScreen__logo"
+          src="https://assets.brand.microsites.netflix.io/assets/493f5bba-81a4-11e9-bf79-066b49664af6_cm_1440w.png?v=49"
+          alt=""
+        />
+      </div>
     </div>
   );
 }
