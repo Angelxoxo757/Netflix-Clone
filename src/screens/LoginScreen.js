b0d4c3edd5e0d65b07f3603079ae3a718a2279b4
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-07 20:16:12 -0500

    Building Netflix Clone LoginScreen

---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./screens/LoginScreen.css";
+
+function LoginScreen() {
+  return (
+    <div>
+      <h1>LoginScreen</h1>
+    </div>
+  );
+}
+
+export default LoginScreen;
