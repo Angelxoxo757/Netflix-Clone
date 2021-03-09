df189ea4e9f1b0ce56519e07c905d2c655c8bc9d
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-09 17:34:08 -0500

    Completed Building Netflix Clone SignupScreen

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -4,11 +4,15 @@ import "./SignupScreen.css";
 function SignupScreen() {
   return (
     <div className="signupScreen">
-      <h1>Sign In</h1>
       <form>
+        <h1>Sign In</h1>
         <input type="email" placeholder="E-mail" />
         <input type="password" placeholder="Password" />
         <button type="submit">Sign In</button>
+        <h4>
+          <span className="signupScreen__gray">New to Netflix? </span>
+          <span className="signupScreen__link">Sign Up Now.</span>
+        </h4>
       </form>
     </div>
   );
