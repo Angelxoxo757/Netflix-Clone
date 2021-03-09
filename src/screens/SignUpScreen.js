ad7cf64d7e60b40e9d7cebaf5593c8225116d50e
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-09 18:59:33 -0500

    Building Netflix Clone Authenication

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -2,8 +2,13 @@ import React from "react";
 import "./SignupScreen.css";
 
 function SignupScreen() {
-  const register = () => {
-    // AUTHENICATION SETUP
+  // AUTHENICATION SETUP
+  const register = (e) => {
+    e.preventDefault();
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -12,10 +17,14 @@ function SignupScreen() {
         <h1>Sign In</h1>
         <input type="email" placeholder="E-mail" />
         <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <button type="submit" onClick={signIn}>
+          Sign In
+        </button>
         <h4>
           <span className="signupScreen__gray">New to Netflix? </span>
-          <span className="signupScreen__link">Sign Up Now.</span>
+          <span className="signupScreen__link" onClick={register}>
+            Sign Up Now.
+          </span>
         </h4>
       </form>
     </div>
