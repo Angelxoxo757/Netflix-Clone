4918d952e9311c72b77a7a5f4c60ca391aa2f2a8
Maria Cromety <mariacromety@Marias-MacBook-Pro.local>
2021-03-12 18:00:44 -0500

    Completed Building Netflix Clone ProfileScreen

--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import "./Nav.css";
+import { useHistory } from "react-router-dom";
 
 function Nav() {
   // USING BEM
   const [show, handleShow] = useState(false);
+  const history = useHistory();
 
   const trasitionNavbar = () => {
     if (window.scrollY > 100) {
@@ -21,11 +23,13 @@ function Nav() {
     <div className={`nav ${show && "nav__black"}`}>
       <div className="nav__contents">
         <img
+          onClick={() => history.push("/")}
           className="nav__logo"
           src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         />
         <img
+          onClick={() => history.push("/profile")}
           className="nav__avatar"
           src="https://i0.wp.com/jimheath.tv/wp-content/uploads/2019/04/lucifer-season-4-netflix.jpg?ssl=1"
           alt=""
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,8 +1,14 @@
 import React from "react";
+import Nav from "../Nav";
 import "./ProfileScreen.css";
 
 function ProfileScreen() {
-  return <div></div>;
+  return (
+    <div className="profileScreen">
+      <Nav />
+      <div className="profileScreen__body"></div>
+    </div>
+  );
 }
 
 export default ProfileScreen;
